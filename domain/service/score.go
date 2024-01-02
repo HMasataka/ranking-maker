f26a5e70395dd62d0b6ead1a35a71b6f9a243eda
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-01-02 16:45:37 +0900

    Feat usecase to service

--- domain/service/score.go
+++ domain/service/score.go
@@ -1,4 +1,4 @@
-package usecase
+package service
 
 import (
 	"context"
@@ -9,27 +9,27 @@ import (
 	"github.com/HMasataka/transactor/redis"
 )
 
-type ScoreUseCase interface {
+type ScoreService interface {
 	Add(ctx context.Context, key, member string) error
 	Count(ctx context.Context, key string, expired time.Duration) (int64, error)
 }
 
-type scoreUseCase struct {
+type scoreService struct {
 	redisTransactor transactor.Transactor
 	scoreRepository repository.ScoreRepository
 }
 
-func NewScoreUseCase(
+func NewScoreService(
 	redisConnectionProvider redis.ConnectionProvider,
 	scoreRepository repository.ScoreRepository,
-) ScoreUseCase {
-	return &scoreUseCase{
+) ScoreService {
+	return &scoreService{
 		redisTransactor: redis.NewTransactor(redisConnectionProvider),
 		scoreRepository: scoreRepository,
 	}
 }
 
-func (c *scoreUseCase) Count(ctx context.Context, key string, expired time.Duration) (int64, error) {
+func (c *scoreService) Count(ctx context.Context, key string, expired time.Duration) (int64, error) {
 	var count int64
 
 	if err := c.redisTransactor.Required(ctx, func(ctx context.Context) error {
@@ -47,7 +47,7 @@ func (c *scoreUseCase) Count(ctx context.Context, key string, expired time.Durat
 	return count, nil
 }
 
-func (c *scoreUseCase) Add(ctx context.Context, key, member string) error {
+func (c *scoreService) Add(ctx context.Context, key, member string) error {
 	return c.redisTransactor.Required(ctx, func(ctx context.Context) error {
 		return c.scoreRepository.Add(ctx, key, member)
 	})
