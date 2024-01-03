a90d13d953b49d78c0a62b35854bd08c5e9491dd
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-01-03 16:30:31 +0900

    Disable redis transaction

--- domain/service/score.go
+++ domain/service/score.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/HMasataka/ranking-maker/domain/repository"
-	"github.com/HMasataka/transactor"
 	"github.com/HMasataka/transactor/redis"
 )
 
@@ -15,7 +14,6 @@ type ScoreService interface {
 }
 
 type scoreService struct {
-	redisTransactor transactor.Transactor
 	scoreRepository repository.ScoreRepository
 }
 
@@ -24,31 +22,14 @@ func NewScoreService(
 	scoreRepository repository.ScoreRepository,
 ) ScoreService {
 	return &scoreService{
-		redisTransactor: redis.NewTransactor(redisConnectionProvider),
 		scoreRepository: scoreRepository,
 	}
 }
 
 func (c *scoreService) Count(ctx context.Context, key string, expired time.Duration) (int64, error) {
-	var count int64
-
-	if err := c.redisTransactor.Required(ctx, func(ctx context.Context) error {
-		var err error
-		count, err = c.scoreRepository.Count(ctx, key, expired)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}); err != nil {
-		return count, err
-	}
-
-	return count, nil
+	return c.scoreRepository.Count(ctx, key, expired)
 }
 
 func (c *scoreService) Add(ctx context.Context, key, member string) error {
-	return c.redisTransactor.Required(ctx, func(ctx context.Context) error {
-		return c.scoreRepository.Add(ctx, key, member)
-	})
+	return c.scoreRepository.Add(ctx, key, member)
 }
