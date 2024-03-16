7c7f9b7f9e7f1113d953874705d0579db2848329
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-03-16 11:53:59 +0900

    Feat created at

--- domain/entity/item.go
+++ domain/entity/item.go
@@ -1,11 +1,14 @@
 package entity
 
 import (
+	"time"
+
 	"github.com/goccy/go-json"
 )
 
 type Item struct {
-	ID string `json:"id" yaml:"id" toml:"id"`
+	ID        string    `json:"id" yaml:"id" toml:"id"`
+	CreatedAt time.Time `json:"created_at" yaml:"created_at" toml:"created_at"`
 }
 
 func (r *Item) MarshalBinary() ([]byte, error) {
